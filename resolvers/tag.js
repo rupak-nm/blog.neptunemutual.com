4c57e54841dc31f85e1a72292237fafaa369cbd6
binodnpm <tokyo@neptunemutual.com>
2022-12-25 14:33:58 +0545

    Added rewrites for missing tags

--- resolvers/tag.js
+++ resolvers/tag.js
@@ -1,3 +1,9 @@
+
+const rewrites = {
+  'analysis-report': 'exploit-analysis',
+  'solana-projects': 'solana'
+}
+
 const resolve = (url) => {
   if (!url) {
     return
@@ -7,10 +13,19 @@ const resolve = (url) => {
     return
   }
 
-  const tag = url.split('/').pop()
+  let tag = url.split('/').pop()
+
+  const rewrite = rewrites[tag]
+
+  if (rewrite) {
+    tag = rewrite
+  }
 
   const { REDIRECT_DOMAIN } = process.env
   return new URL(`blog/tag/${tag}#${tag}`, REDIRECT_DOMAIN).href
 }
+
+// process.env.REDIRECT_DOMAIN = 'https://neptunemutual.com'
+// console.log(resolve('tag/solana-projects'))
 
 module.exports = { resolve }
